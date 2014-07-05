b178ceac6969be63b6db0980514236c021916f61
theptrk <patrick.tran06@gmail.com>
2014-07-05 14:13:44 -0700

    Implemented linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function(){
+    list.head = list.head.next;
   };
 
   list.contains = function(target, node){
+
+    node = node || this.head;
+
+    if (node.value === target) { return true; }
+    if (node.next !== null) {
+      return this.contains(target, node.next);
+    }
+
+    return false;
   };
 
   return list;
