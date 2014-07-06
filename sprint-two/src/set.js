dc11da30300a7c51ceb83619c0bc61a7b637adca
theptrk <patrick.tran06@gmail.com>
2014-07-05 17:32:08 -0700

    Added add funciton

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,12 +1,13 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+    return this._storage[item] = true;
 };
 
 setPrototype.contains = function(item){
