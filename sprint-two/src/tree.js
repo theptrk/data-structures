792e4aacb341ff0c173fd622a3361a72092421f2
theptrk <patrick.tran06@gmail.com>
2014-07-05 17:30:29 -0700

    Implemented the tree structure with extend

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,9 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  newTree.extend(treeMethods);
+
   return newTree;
 };
 
@@ -9,8 +11,28 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+    var newTree = makeTree(value);
+    return this.children.push(newTree);
 };
 
 treeMethods.contains = function(target){
+    var found = false;
+
+    var traverse = function(obj){
+        if (obj.value === target) { found = true; }
+        for (var i = 0; i < obj.children.length; i++) {
+            traverse(obj.children[i]);
+        }
+    };
+
+    traverse(this);
+    return found;
 };
 
+Object.prototype.extend = function(obj) {
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            this[prop] = obj[prop];
+        }
+    }
+};
