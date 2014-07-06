5c1a0f67802b4af13fa15075d943b208985c16d0
theptrk <patrick.tran06@gmail.com>
2014-07-05 17:56:51 -0700

    Added insert and retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,10 +5,20 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  this._storage[i] = this._storage[i] || [];
+  this._storage[i].push([k,v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  
+  for (var j = 0; j < this._storage[i].length; j++) {
+      if (this._storage[i][j][0] === k) {
+        return this._storage[i][j][1];
+      } 
+  }
+  return false;
 };
 
 HashTable.prototype.remove = function(k){
