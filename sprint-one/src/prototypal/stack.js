97083586b0eb133da3996ffc9daeaf377e238c8b
theptrk <patrick.tran06@gmail.com>
2014-07-05 20:58:00 -0700

    Implemented prototypcal queue and stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,25 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+
+  instance.storage = {};
+  instance.sizeOf = 0;
+
+  return instance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  this.storage[this.sizeOf++] = value;
+};
+
+stackMethods.pop = function(){
+  this.sizeOf && this.sizeOf--;
+  var value = this.storage[this.sizeOf];
+  delete this.storage[this.sizeOf];
+  return value;
+};
+
+stackMethods.size = function(){
+  return this.sizeOf;
+};
