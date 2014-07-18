cb2247dd8b04965134972043b2652b6340ecffee
theptrk <patrick.tran06@gmail.com>
2014-07-17 22:25:29 -0700

    linked list methods: count_recursibe and self_recursive

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -19,10 +19,11 @@ var makeLinkedList = function(){
     list.head = list.head.next;
   };
 
-  // This linked list is implemted as a singly linked list and due to the
-  // lack of a reference to the previous node, we must initiate a contains 
-  // function with the head as the starting point of our search. All nodes
-  // are visited until we reach a node containing our target or we return false.
+  // This linked list is implemeted as a singly linked list and due to the
+  // lack of a reference to the previous node, we must by default initiate a 
+  // contains function with the head as the starting point of our search. 
+  // All nodes are visited until we reach a node containing our target or 
+  // we return false.
   list.contains = function(target, node){
 
     node = node || this.head;
@@ -35,6 +36,34 @@ var makeLinkedList = function(){
     return false;
   };
 
+  // recursive counting function to count all the nodes
+  list.count_recursive = function() {
+    var counter = 0;
+    var traverseNodes = function(node){
+      counter++;
+      if (node.next) {
+        traverseNodes(node.next);
+      }
+    };
+    traverseNodes(list.head);
+    return counter;
+  };
+
+  // self recursive counting function to count all nodes 
+  list.count_self_recursive = function(node, count) {
+    if (list.head === null) { return null; }
+    
+    node = node || list.head;
+    count = count || 0;
+
+    count++;
+    if (node.next) {
+      return list.count_recursive(node.next, count);
+    } else {
+      return count;
+    }
+  };
+
   return list;
 };
 
@@ -45,3 +74,9 @@ var makeNode = function(value){
 
   return node;
 };
+
+linkedList = makeLinkedList();
+linkedList.addToTail(4);
+linkedList.addToTail(4);
+linkedList.addToTail(4);
+console.log(linkedList.count_recursive()); 
