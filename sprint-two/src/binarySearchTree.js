f6652681e3adaf70a1cdcceea413f4cf87fed3c5
theptrk <patrick.tran06@gmail.com>
2014-07-18 15:35:12 -0700

    Added the insert functionality

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,37 @@
 var makeBinarySearchTree = function(value){
+    var binarySearchTree = {};
+    binarySearchTree.value = value;
+    binarySearchTree.left = null;
+    binarySearchTree.right = null;
+    
+    _.extend(binarySearchTree, binaryTreeMethods);
+
+    return binarySearchTree;
+};
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value, node) {
+
+  node = node || this;
+
+  if (value > node.value) {
+    if (node.right === null) {
+      node.right = makeBinarySearchTree(value);
+    } else {
+      this.insert(value, node.right);
+    }
+  } else {
+    if (node.left === null) {
+      node.left = makeBinarySearchTree(value);
+    } else {
+      this.insert(value, node.left);
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(value) {
+};
+
+binaryTreeMethods.depthFirstLog = function(value) {
 };
